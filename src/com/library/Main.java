158ba9b8266875696e81af6e35e1dfcda19ecccf
Adam Lamoglia <adamrammensee@gmail.com>
2018-01-23 10:03:45 -0300

    Add files via upload

--- src/com/library/Main.java
+++ src/com/library/Main.java
@@ -1,8 +1,10 @@
-package com.library;
+package library;
+
+import comando.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+	
+	public static void main(String [] args){
+		
+	}
 }
---
+++ src/com/library/Sistema.java
@@ -0,0 +1,42 @@
+package library;
+
+import java.util.HashMap;
+import comando.*;
+
+public class Sistema {
+	private HashMap comandos;
+	
+	public Sistema(){
+		comandos = new HashMap();
+		
+		
+	}
+	
+	private void iniciarComandos(){
+		//Emprestar livro
+		comandos.put("emp", EmprestimoCommand.getInstancia());
+		
+		//Devolver livro
+		comandos.put("dev", DevolucaoCommand.getInstancia());
+		
+		//Reservar livro
+		comandos.put("res", ReservaCommand.getInstancia());
+		
+		//Observar livro
+		comandos.put("obs", ObservacaoCommand.getInstancia());
+		
+		//Obter Informações do livro
+		comandos.put("liv", InformacaoCommand.getInstancia());
+		
+		//Obter quantidade de notificações
+		comandos.put("ntf", NotificacaoCommand.getInstancia());
+		
+		//Obter historico do usuário
+		comandos.put("usu", HistoricoCommand.getInstancia());
+		
+		//Sair
+		comandos.put("sai", SaidaCommand.getInstancia());
+	}
+	
+	
+}
