62dd8db220ff8219f1bd50c5fc24a0dc88e34150
Adam Lamoglia <adamrammensee@gmail.com>
2018-01-25 22:37:16 -0300

    Inicialização do padrão State em exemplares

--- src/com/state/ExemplarDisponivel.java
+++ src/com/state/ExemplarDisponivel.java
@@ -1,5 +1,23 @@
 package state;
 
-public class ExemplarDisponivel {
+public class ExemplarDisponivel implements EstadoExemplar {
+
+	@Override
+	public void emprestarExemplar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void devolverExemplar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reservarExemplar() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/com/state/ExemplarIndisponivel.java
+++ src/com/state/ExemplarIndisponivel.java
@@ -1,5 +1,23 @@
 package state;
 
-public class ExemplarIndisponivel {
+public class ExemplarIndisponivel implements EstadoExemplar {
+
+	@Override
+	public void emprestarExemplar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void devolverExemplar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reservarExemplar() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
