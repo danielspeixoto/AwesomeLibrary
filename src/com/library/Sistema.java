d52e6d76d6a63432ac3d8cf4d41e859995175490
Adam Lamoglia <adamrammensee@gmail.com>
2018-01-25 22:36:23 -0300

    modificação no construtor de Sistema

--- src/com/library/Sistema.java
+++ src/com/library/Sistema.java
@@ -5,19 +5,19 @@ import java.util.HashMap;
 
 import book.*;
 import comando.*;
+import fachada.Facade;
 import user.*;
 
 public class Sistema {
 	private HashMap comandos;
-	private ArrayList usuarios;
 	
 	public Sistema(){
 		comandos = new HashMap();
-		usuarios = new ArrayList();
 		
-		this.iniciarComandos();
-		this.registraUsuarios();
-				
+		//inicializa livros e usuarios
+		Facade.getInstancia().registrarUsuarios();
+		Facade.getInstancia().registrarLivros();
+		Facade.getInstancia().registrarExemplares();
 	}
 	
 	private void iniciarComandos(){
@@ -46,33 +46,7 @@ public class Sistema {
 		//comandos.put("sai", SaidaCommand.getInstancia());
 	}
 	
-	private void registraUsuarios(){
-		//TODO retirar dependencia de Usuario com Sistema
-		usuarios.add(new Funcionario("123","João da Silva"));
-		usuarios.add(new Aluno("456","Luiz Fernando Rodrigues"));
-		usuarios.add(new Funcionario("789","Pedro Paulo"));
-		usuarios.add(new Professor("100","Carlos Lucena"));
-	}
-	
-	private void registraExemplares(){
-		new Exemplar("100","Engenharia de Software","AddisonWesley","Ian Sommervile","6a","2000","01");
-		new Exemplar("100","Engenharia de Software","AddisonWesley","Ian Sommervile","6a","2000","02");
-		
-		new Exemplar("101","UML – Guia do Usuário","Campus","Grady Booch,James Rumbaugh,Ivar Jacobson","7a","2000","03");
-		
-		new Exemplar("200","Code Complete","Microsoft Press","Steve McConnell","2a","2014","04");
-		
-		new Exemplar("201","Agile SoftwareDevelopment,Principles,Patterns,and Practices","Prentice Hall","Robert Martin","1a","2002","05");
-		
-		new Exemplar("300","Refactoring:Improving the Design of Existing Code","Addison-Wesley Professional","Martin Fowler","1a","1999","06");
-		new Exemplar("300","Refactoring:Improving the Design of Existing Code","Addison-Wesley Professional","Martin Fowler","1a","1999","07");
-		
-		//livro com exemplar nao disponivel
-		new Livro("301","Software Metrics: A Rigorous and Practical Approach","CRC Press","Norman Fenton, James Bieman","3a","2014");
-		
-		new Exemplar("400","UML Distilled: A Brief Guide to the Standard Object Modeling Language","Addison-Wesley Professional","Martin Fowler","3a","2003","08");
-		new Exemplar("400","Engenharia de Software","AddisonWesley","Ian Sommervile","6a","2000","09");
-	}
+
 	
 	public void servico(String comando, Dados dado){
 		
