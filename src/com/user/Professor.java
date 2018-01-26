7cc139d268a084b8843af16f13eb9081625ae897
Adam Lamoglia <adamrammensee@gmail.com>
2018-01-25 23:19:56 -0300

    Implementação do padrão observer

--- src/com/user/Professor.java
+++ src/com/user/Professor.java
@@ -1,10 +1,34 @@
 package user;
 
-public class Professor extends Usuario {
+import java.util.ArrayList;
+
+import book.Livro;
+import observer.*;
+
+public class Professor extends Usuario implements Observer {
+	
 	private final int tempoDeEmprestimo = 7;
+	private int notificacoesDeReserva;
+	private Subject livroObservado;
 	
 	public Professor(String id, String nome) {
 		super(id, nome);
+		
+		this.notificacoesDeReserva = 0;
 	}
+	
+	public void registrarObservadorDeLivro(Livro livro){
+		livroObservado.registerObservers(this);
+	}
+
+	@Override
+	public void update() {
+		this.notificacoesDeReserva++;
+		
+		System.out.println("Professor "+this.getNome()+" notificado "+this.notificacoesDeReserva+" vezes.");
+		
+	}
+
+
 	
 }
