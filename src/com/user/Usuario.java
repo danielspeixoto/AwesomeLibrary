34629b52f5bcfefa015047a29e6aa0b82afb62d9
Adam Lamoglia <adamrammensee@gmail.com>
2018-01-25 22:38:56 -0300

    Adiciona atributos relacionado a emprestimo, reserva e historico, e criação do metodo para verificar se existe uma reserva igual
    Atenção, verificar se realmente este método existirá no projeto

--- src/com/user/Usuario.java
+++ src/com/user/Usuario.java
@@ -2,13 +2,43 @@ package user;
 
 import java.util.ArrayList;
 
+import book.Livro;
+import funcionalidades.*;
+
 public abstract class Usuario {
 	private final String id;
 	private final String nome;
+	private ArrayList<Reserva> reservas;
+	private ArrayList<Emprestimo> emprestimos;
+	private ArrayList<Historico> historico;
+	
 	
 	public Usuario(String id, String nome){
 		this.id = id;
 		this.nome = nome;
+		reservas = new ArrayList();
+		emprestimos = new ArrayList();
+		historico = new ArrayList();
+	}
+	
+	public int getQuantidadeDeReservas(){
+		return reservas.size();
+	}
+	
+	public void adicionaReserva(Livro livro){
+		
+	}
+	
+	public boolean existeReservaDoMesmoLivro(Livro livro){
+		for(int i = 0; i < reservas.size(); i++)
+			if(reservas.get(i).getLivroAssociado().getId().equals(livro.getId()))
+				return true;
+		
+		return false;		
+	}
+
+	public ArrayList<Reserva> getReservas() {
+		return reservas;
 	}
 
 	public String getId() {
