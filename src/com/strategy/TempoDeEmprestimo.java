352b0e0899aaa4c89ac4758f269bf79b580cba3e
Adam Lamoglia <adamrammensee@gmail.com>
2018-01-30 21:26:36 -0300

    Add files via upload

---
+++ src/com/strategy/TempoDeEmprestimo.java
@@ -0,0 +1,11 @@
+package strategy;
+
+import java.util.Calendar;
+
+import funcionalidades.Emprestimo;
+
+public interface TempoDeEmprestimo {
+	public Calendar getDataDevolucao(Emprestimo emprestimo);
+	public int getPeriodoDeEmprestimo();
+
+}
